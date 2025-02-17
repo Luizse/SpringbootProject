7ff16bf588213c593b6ee9efc9f798cd4ad0d6c8
Luizse <88157931+Luizse@users.noreply.github.com>
2025-02-17 19:15:33 +0530

    Add files via upload

---
+++ mycoolapp/01-spring-boot/src/main/java/com/luv2code/springboot/start/mycoolapp/rest/ToyRest.java
@@ -0,0 +1,23 @@
+package com.luv2code.springboot.start.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ToyRest {
+    @GetMapping("/")
+    public List<String> getToy(){
+        return List.of("teddy","doggy","car");
+    }
+
+    @GetMapping("/DailyRoutine")
+    public List<String> getDailyworkwout(){
+        return List.of("6:30 to 7:30 = workout","8:30 to 9:30 = Study");
+    }
+    @GetMapping("/workout")
+    public String getworkout(){
+        return "Yay its sunday workout time";
+    }
+}
