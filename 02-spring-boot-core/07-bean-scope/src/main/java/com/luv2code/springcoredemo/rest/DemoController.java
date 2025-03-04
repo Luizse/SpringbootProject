dec5a3e11f4658fcd2cf14448280f31c4bde71b6
Luizse <88157931+Luizse@users.noreply.github.com>
2025-03-04 09:59:42 +0530

    Add files via upload

---
+++ 02-spring-boot-core/07-bean-scope/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,31 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach MyCoach;
+    private Coach anotherOne;
+    @Autowired
+    public DemoController(@Qualifier("CricketCoach") Coach theCoach,
+                          @Qualifier("CricketCoach") Coach thAnotherOne){
+        System.out.println("In contructor: " + getClass().getSimpleName());
+        MyCoach = theCoach;
+        anotherOne = thAnotherOne;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return MyCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing beans: myCoach == anotherCoach," + (MyCoach == anotherOne);
+        }
+}
